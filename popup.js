cf72cfa6611b89bf3b4692293dae8beb726c4589
chaeyeonlee07 <lcy2507@naver.com>
2023-12-28 17:11:02 -0800

    fixed background

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
-
 $(document).ready(function() {
+  let selectedDate; // Declare selectedDate in a wider scope
+
   $("#saveButton").on("click", function() {
     goToRandomQuote();
+    saveData();
   });
 
   $("#editButton").on("click", function() {
@@ -11,82 +13,61 @@ $(document).ready(function() {
   function goToPick() {
     $('#calendar').hide();
     $('#pick').fadeIn(800);
+    generateRandomEntries();
   }
 
   function goToRandomQuote() {
     $('#pick').hide();
     $('#calendar').fadeIn(800);
+    generateRandomEntries(); 
+  }
+
+  function getDate() {
+    const today = new Date();
+    const month = today.getMonth();
+    const day = today.getDate();
+    const year = today.getFullYear();
+    selectedDate = {
+      "month": month,
+      "day": day,
+      "year": year
+    };
+    return selectedDate;
+  }
+
+  function generateRandomEntries() {
+    chrome.storage.local.get(null, function (result) {
+      const entryCount = Object.keys(result).length;
+      console.log('Number of entries in local storage:', entryCount);
+    }); 
+  }
+
+  // Function to convert image to Base64
+  function getBase64Image(file, callback) {
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      callback(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  function saveData() {
+    const happyMoment = document.getElementById('happyInput').value;
+    const quoteOfTheDay = document.getElementById('quoteInput').value;
+
+    const bannerImage = document.getElementById('photoInput').files[0];
+    getBase64Image(bannerImage, function(imgData) {
+      const today = getDate();
+      const dataToSave = {
+        "happyMoment": happyMoment,
+        "quoteOfTheDay": quoteOfTheDay,
+        "uploadedPhoto": imgData
+      };
+
+      console.log(dataToSave);
+      chrome.storage.local.set({ [today]: dataToSave });
+    });
   }
 });
-
-// function generateDate() {
-//   // Get today's date
-//   const today = new Date();
-//   month = today.getMonth();
-//   day = today.getDate();
-//   year = today.getFullYear();
-// }
-
-
-// //background 
-// function RandomToWritebg() {
-//     // Change the source of bg_1 to bg_2 when saveButton is clicked
-//     document.getElementById('bg').src = 'art/bg_2.png';
-// }
-
- 
-// //save data document.getElementById('saveButton').addEventListener('click', function() {
- 
-// function saveLog() {
-    
-//         generateDate(); 
-      
-//         // Get the user's inputs
-//         const happyMoment = document.getElementById('happyInput').value;
-//         const quoteOfTheDay = document.getElementById('quoteInput').value;
-//         const uploadedPhoto = document.getElementById('photoInput').files[0]; // For file input
-      
-//         const selectedDate = {
-//           month: month,
-//           day: day,
-//           year: year
-//         };
-      
-//         // Prepare data to save in Chrome storage (handle uploadedPhoto accordingly)
-//         const dataToSave = {
-//           happyMoment: happyMoment,
-//           quoteOfTheDay: quoteOfTheDay,
-//           uploadedPhoto: uploadedPhoto,  
-//           selectedDate: selectedDate
-//         };
-       
-//         chrome.storage.local.set({ selectedDate: dataToSave });
-//         isLogPage = False; 
-//         RandomToWritebg();
-//       }
-
-// //change the data
-// function redo() {
-//   writeToRandombg();
-//   isLogPage = True; 
-// }
-
-
-// function getRandomLog() {
-
-//   chrome.storage.local.get(null, function (result) {
-
-//     const savedLogs = Object.values(result);
-    
-
-//     const randomIndex = Math.floor(Math.random() * savedLogs.length);
-//     const randomEntry = savedLogs[randomIndex];
-    
-
-//     console.log('Randomly selected log:', randomEntry);
-//   });
-// }
- 
- 
 
  
